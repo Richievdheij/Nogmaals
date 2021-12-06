affb2edbb59b564273369bf7d868ba140df686ca
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2021-12-06 11:53:55 +0100

    loop5Resultaat.py
    Herhaal een input tot het resultaat daarvan gelijk is aan 'quit'

--- Duizelig/loop6Cijfers.py
+++ Duizelig/loop6Cijfers.py
@@ -1,6 +1,6 @@
 # Tel de cijfers vanaf 50 totdat hun gezamelijke som groter is dan 1000, print elk cijfer en de totale som per iteratie.
 
-i = 1
-while i <= 30:
+i = 50
+while i <= 1000:
     print(i)
     i += 1
