7703dc0ea7d1bee2440901e6ebfd6e1465e08e46
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2021-12-07 10:38:22 +0100

    example7.py
    Update

--- robotarm-python-2021-main kopie/example7.py
+++ robotarm-python-2021-main kopie/example7.py
@@ -1,26 +1,22 @@
+## Oefening 7
+
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 6')
+robotArm = RobotArm('exercise 7')
 
 # Jouw python instructies zet je vanaf hier:
 
-robotArm.speed = 3
+robotArm.speed = 1
 
-for i in range(7):
+for x in range(5):
     robotArm.moveRight()
-
-
-for i in range(8):
-
-    robotArm.grab()
-
-    robotArm.moveRight()
-
-    robotArm.drop()
-
-    for i in range(2):
+    
+    for y in range(6):
+        robotArm.grab()
         robotArm.moveLeft()
+        robotArm.drop()
+        robotArm.moveRight()
+    robotArm.moveRight()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
-
